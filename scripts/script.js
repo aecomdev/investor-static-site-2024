5c46905abbac43725893efc9445dc0d3a78365d1
micposso <micposso@gmail.com>
2024-03-18 16:38:42 -0400

    add JS license

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,20 @@
 new fullpage("#fullpage", {
-  showActiveTooltip: true, 
-  slidesNavigation: true, 
+  licenseKey: "YV7M9-KK2H6-IVEB6-N09QH-NMNZL",
+  scrollHorizontallyKey: "VzZZV1ZqYjIwdVkyOXRJT19JRzdjMk55YjJ4c1NHOXlhWHB2Ym5SaGJHeDV6eDg=",
+  showActiveTooltip: true,
   slidesNavPosition: "bottom",
-  controlArrows: false, 
-  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection', 'sixthSection'],
-  menu: "#menu", 
-  scrollingSpeed: 700, 
-  css3: true, 
-  loopHorizontal: true, 
+  controlArrows: false,
+  anchors: [
+    "firstSection",
+    "secondSection",
+    "thirdSection",
+    "fourthSection",
+    "fifthSection",
+    "sixthSection",
+  ],
+  menu: "#menu",
+  scrollingSpeed: 700,
+  css3: true,
+  loopHorizontal: true,
 });
 
