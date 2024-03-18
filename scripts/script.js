2ba7a8e5054f531274b489a0940c94ed0413fa24
micposso <micposso@gmail.com>
2024-03-18 15:44:28 -0400

    vertical nav

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+new fullpage("#fullpage", {
+  showActiveTooltip: true, 
+  slidesNavigation: true, 
+  slidesNavPosition: "bottom",
+  controlArrows: false, 
+  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection', 'sixthSection'],
+  menu: "#menu", 
+  scrollingSpeed: 700, 
+  css3: true, 
+  loopHorizontal: true, 
+});
+
